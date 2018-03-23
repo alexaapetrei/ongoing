1f9cf620b21484bb895fb7a84de8eb15d1038ebc
Alex Apetrei <alexaapetrei@gmail.com>
2018-03-23 13:59:34 +0200

    first push
    little repo for continuous deployment

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Soo cool</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+</head>
+<body>
+<h1><?php echo "so , this is continuous deployment" ?></h1>
+</body>
+</html>
